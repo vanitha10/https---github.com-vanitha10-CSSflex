c2be4da3ddc4a87fe94398dceb85871a6bcf127e
vanitha1 <vanitha.selva@gmail.com>
2022-03-02 21:45:29 -0600

    hj

---
+++ Javascript/app.js
@@ -0,0 +1,11 @@
+
+let x= 10
+let y="hello"
+
+let z= true
+let array = ['apple','mango']
+let object={name :'Gowtham' , role : "web dev" }
+
+
+
+console.log()
---
+++ mahen.js
@@ -0,0 +1,67 @@
+/*let age=15
+console.log(typeof age);*/
+/*
+let a=15, b= 10;
+if (a>b)
+
+{
+ let c = 50;
+}
+
+else{
+    let c = 100;
+
+}
+console.log(c);*/
+
+
+
+
+
+/* var user="John";
+
+var user="Mike";
+
+console.log(user); */
+
+/*let userProfile;*/
+
+
+
+
+
+/*const COLOR = "red";
+ 
+COLOR = "blue";
+
+console.log(COLOR);
+
+
+console.log("Test"/0);*/
+
+/* let a=10, b=20;
+
+
+
+console.log(`Sum of a and b is:${a+b}`); */
+
+console.log(5==="5");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
