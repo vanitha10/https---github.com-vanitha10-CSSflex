d03d39b957bfa43d65f3f0e51562df0d378122bd
vanitha1 <vanitha.selva@gmail.com>
2022-03-06 22:41:12 -0600

    hello

---
+++ random projects/javascript.js
@@ -0,0 +1,99 @@
+
+
+/* let person={
+
+    firstName: 'John',
+    lastName: 'Doe',
+greet(){
+
+
+        console.log("hello");
+
+
+    }
+};
+person.greet(); */
+
+
+/* let person = {
+console.log(typeof person); */
+
+/* let person={
+
+
+    firstName: 'Shane',
+    lastName:'Warne',
+
+};
+
+console.log(person.firstName + " "+ person.lastName);
+person.country ='Aus';
+console.log(person);
+ */
+/* 
+let person={
+
+
+    firstName: 'Shane',
+    lastName:'Warne',
+
+};
+
+console.log(person.firstName + " "+ person.lastName);
+
+person.country='Aus';
+console.log("before:", person);
+
+delete person.country;
+console.log("After:" , person);
+
+console.log('lastName' in person) */
+
+
+
+
+/* for(const key in person){
+
+
+    console.log(key
+        );
+} */
+
+/* let person={
+
+    firstName: 'Shane'
+}  */
+/* 
+function Person(firstName, lastName, age){
+
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.age=age;
+
+}
+
+let person1 =new Person('Peter','waugh',34);
+let person2 =new Person('parkeer','doemh',31);
+
+let person3 =new Person('Phonr','waugher',38);
+console.log(person3.age); */
+
+/* 
+function range(int javascript)
+
+{
+    
+    int a=5;
+    for(int i=0;i<javascript;i++)
+
+    {
+
+        console.log(a)
+    }
+
+}range(3); */
+ var quiz=[1,2,3];
+ var js=[6,7,8];
+ var result=quiz.concat(js);
+ document.writeln(result);
+ 
